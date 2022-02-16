6214ed779aeb3a9c3bf6596d844d631a1410a8dd
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-16 17:39:27 -0500

    material list tested

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -8,6 +8,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -18,6 +20,8 @@ const MaterialComponents = [
   MatToolbarModule,
   MatSidenavModule,
   MatMenuModule,
+  MatListModule,
+  MatDividerModule,
 ];
 
 @NgModule({
