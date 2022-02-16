e26c6b9220e2888082c1d158e14c148a083b17c6
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-16 11:25:22 -0500

    material badge tested

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatBadgeModule } from '@angular/material/badge';
 
 const MaterialComponents = [
   MatButtonModule,
   MatIconModule,
   MatButtonToggleModule,
+  MatBadgeModule,
 ];
 
 @NgModule({
