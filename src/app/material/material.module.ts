81e3a9ad03540b0b2734bfb4ae5d1d744d3158a7
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-16 11:06:34 -0500

    material toggle button tested

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
-const MaterialComponents = [MatButtonModule, MatIconModule];
+const MaterialComponents = [
+  MatButtonModule,
+  MatIconModule,
+  MatButtonToggleModule,
+];
 
 @NgModule({
   imports: MaterialComponents,
