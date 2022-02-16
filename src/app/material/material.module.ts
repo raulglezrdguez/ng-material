1f2835c9d68dbeca85e985a67fb2e1f50cf0a1b1
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-16 10:42:36 -0500

    material button tested

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
-const MaterialComponents = [MatButtonModule];
+const MaterialComponents = [MatButtonModule, MatIconModule];
 
 @NgModule({
   imports: MaterialComponents,
