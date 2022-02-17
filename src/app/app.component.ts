e435ae16f486d84dc97bc8867d956ddf1a5bcb75
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-17 15:21:50 -0500

    material datatable filter/sort tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,54 @@
-import { Component } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DialogComponent } from './dialog/dialog.component';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort, Sort } from '@angular/material/sort';
 
 type Option = {
   name: string;
 };
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  constructor(private snackbar: MatSnackBar, private matDialog: MatDialog) {}
+export class AppComponent implements AfterViewInit {
+  constructor(
+    private snackbar: MatSnackBar,
+    private matDialog: MatDialog,
+    private _liveAnnouncer: LiveAnnouncer
+  ) {}
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
 
   title = 'ng-material';
   notifications = 28;
@@ -75,5 +111,29 @@ export class AppComponent {
     dialogRef.afterClosed().subscribe((result) => {
       console.log(`dialog closed: ${result}`);
     });
+  }
+
+  logTableData(row: any) {
+    console.log(row);
+  }
+
+  applyFilter(filter: any) {
+    this.dataSource.filter = (filter.target.value as string)
+      .trim()
+      .toLowerCase();
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    console.log(sortState);
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -26,6 +26,8 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -54,6 +56,8 @@ const MaterialComponents = [
   MatTooltipModule,
   MatSnackBarModule,
   MatDialogModule,
+  MatTableModule,
+  MatSortModule,
 ];
 
 @NgModule({
