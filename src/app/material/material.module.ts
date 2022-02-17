cd23b528805dac1219dbacf9656a949fe75bca39
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-17 08:38:58 -0500

    material card tested

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -12,6 +12,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCardModule } from '@angular/material/card';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -26,6 +27,7 @@ const MaterialComponents = [
   MatDividerModule,
   MatGridListModule,
   MatExpansionModule,
+  MatCardModule,
 ];
 
 @NgModule({
