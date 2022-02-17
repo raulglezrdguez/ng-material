26ccfa5d8cbadff1f6781b96e14d2f24fb87749b
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-17 09:17:44 -0500

    material stepper tested

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -14,6 +14,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatStepperModule } from '@angular/material/stepper';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -30,6 +31,7 @@ const MaterialComponents = [
   MatExpansionModule,
   MatCardModule,
   MatTabsModule,
+  MatStepperModule,
 ];
 
 @NgModule({
