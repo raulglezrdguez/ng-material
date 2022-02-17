d654c0669d86855d6445645c9162b7471bf213e2
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-17 11:28:31 -0500

    material tooltip tested

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -23,6 +23,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -48,6 +49,7 @@ const MaterialComponents = [
   MatRadioModule,
   MatDatepickerModule,
   MatNativeDateModule,
+  MatTooltipModule,
 ];
 
 @NgModule({
